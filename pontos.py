55491133b578229cfa0a68d9440616a5c7b19c36
Henrique Coelho <89171693+HenriqueCMO@users.noreply.github.com>
2022-02-22 22:44:55 -0300

    Add files via upload

---
+++ pontos.py
@@ -0,0 +1,18 @@
+import math
+
+x1 = float(input("Digite a coordenada X1: "))
+y1 = float(input("Digite a coordenada Y1: "))
+x2 = float(input("Digite a coordenada X2: "))
+y2 = float(input("Digite a coordenada Y2: "))
+
+distancia = ((x2-(x1))**2)+((y2-(y1))**2)
+raiz = math.sqrt(distancia)
+
+if raiz >0 and raiz >=10:
+    print("longe")
+else:
+    print("perto")
+
+
+
+
